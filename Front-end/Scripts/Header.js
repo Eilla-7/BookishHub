4ce13344b8d2c26d5c5472de58b32b3c1ae0405f
Eilla-7 <eilafadel2238@gmail.com>
2024-05-01 12:41:16 +0300

    Front-end

---
+++ Front-end/Scripts/Header.js
@@ -0,0 +1,8 @@
+function showMenu(){
+    const menu = document.querySelector('.menu')
+    menu.style.display = 'flex'
+}
+function hideMenu(){
+    const menu = document.querySelector('.menu')
+    menu.style.display = 'none'
+}
